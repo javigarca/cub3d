9b8c06d4df7b2266942f1390f7f9bc12f6d82d9e
xamayuel <xamayuel@Portatil>
2024-02-17 15:47:09 +0100

    gestion de argumentos iniciada

--- includes/cube3d.h
+++ includes/cube3d.h
@@ -6,7 +6,7 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 14:29:34 by xamayuel          #+#    #+#             */
-/*   Updated: 2024/02/17 14:36:00 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/02/17 15:01:30 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,16 @@
 # include "libft.h"
 
 # define VERSION "0.1.0"
-
+/************************** FOREGROUND COLORS *********************************/
+# define KNRM  "\x1B[0m"
+# define KRED  "\x1B[31m"
+# define KGRN  "\x1B[32m"
+# define KYEL  "\x1B[33m"
+# define KBLU  "\x1B[34m"
+# define KMAG  "\x1B[35m"
+# define KCYN  "\x1B[36m"
+# define KWHT   "\x1B[37m"
+# define KNON   "\033[0m"
+# define KBLA   "\x1b[40m"
+/******************************************************************************/
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,60 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 13:45:06 by xamayuel          #+#    #+#             */
-/*   Updated: 2024/02/17 14:30:30 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/02/17 15:44:50 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
+int ft_is_valid_file_extension(const char *filename)
+{
+	int len;
 
+	len =ft_strlen(filename);
+	if (len < 4)
+	{
+		printf("\x1B[31mERROR\n"KWHT"CUBE3D: Map filename too short."KNRM"\n");
+		return (FALSE);
+	}
+	if (ft_strncmp(filename+len-4,".cub",4)==0)
+		return (TRUE);
+	printf(KRED"ERROR\n"KWHT"CUBE3D: Incorrect filename extension."KNRM"\n");	
+	return (FALSE);
+}
+
+
+int ft_is_valid_filename(const char *filename)
+{
+	int fd;
+	// Abrir el fichero en modo lectura
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1) {
+	perror(KRED"Error\n"KWHT"CUBE3D");
+	return (FALSE);
+	}
+	if (close(fd) == -1) {
+	perror(KRED"Error\n"KWHT"CUBE3D");
+	return (FALSE);
+	}
+	return (TRUE);
+
+}
+int ft_is_valid_arguments(char *arguments[])
+{
+	if (ft_is_valid_file_extension(arguments[1]) == FALSE)
+		return (FALSE);
+	if (ft_is_valid_filename(arguments[1]) == FALSE)
+		return (FALSE);
+	return (TRUE);
+}
 int	main(int argn, char *argv[])
 {
-	printf("CUBE-3D %d %s", argn, argv[0]);
+	if (argn != 2)
+		printf("\x1B[31mERROR\n"KWHT"CUBE3D:Incorrect number of arguments."KNRM"\n");
+	else if (ft_is_valid_arguments(argv) == TRUE)
+	{
+		printf("\n puede comenzar el juego\n");
+	}
+	return (0);
 }

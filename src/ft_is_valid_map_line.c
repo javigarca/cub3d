5e89d2f5fc67fe21b06b49dc6768606b54074266
xamayuel <xamayuel@Portatil>
2024-02-18 13:31:25 +0100

    acabado chequeo inicial. falta ordenar, comentar..

--- src/ft_is_valid_map_line.c
+++ src/ft_is_valid_map_line.c
@@ -6,13 +6,22 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 12:15:28 by xamayuel          #+#    #+#             */
-/*   Updated: 2024/02/18 12:56:26 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/02/18 13:25:40 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 
-
+int ft_line_has_only_6(const char *str) {
+  while (*str) {
+    if (*str != '0' && *str != 'N' && *str != 'S' && *str != 'W' && *str != 'E' &&*str != '1' && *str != ' '&& *str != '\n')
+	{
+      return FALSE; // Caracter encontrado
+    }
+    str++;
+  }
+  return TRUE; // No se encontraron caracteres
+}
 int ft_line_has_only_1(const char *str) {
   while (*str) {
     if (*str != '1' && *str != ' '&& *str != '\n')
@@ -31,14 +40,23 @@ int ft_is_valid_first_map_line(const char *line)
 	map_line = ft_strtrim(line, " ");
 	if (ft_line_has_only_1(map_line) == FALSE)
 		return (ft_show_error("Incorrect last line."));
-	printf("%s", map_line);
 	free(map_line);
 	return (TRUE);
 }
 
 int ft_is_valid_inter_map_line(const char *line)
 {
-	printf("%s\n", line);
+	char *map_line;
+
+
+	map_line = ft_strtrim(line, " ");
+	if (ft_line_has_only_6(map_line) == FALSE)
+		return (ft_show_error("Incorrect map line."));
+	if (map_line[0] != '1')
+		return (ft_show_error("Open map line."));
+	if (map_line[ft_strlen(map_line)-2] != '1')
+		return (ft_show_error("Open map line."));
+	free(map_line);
 	return (TRUE);
 }
 
@@ -49,7 +67,6 @@ int ft_is_valid_last_map_line(const char *line)
 
 
 	map_line = ft_strtrim(line, " ");
-	//printf("\n %d\n", ft_line_has_only_1(map_line));
 	if (ft_line_has_only_1(map_line) == FALSE)
 		return (ft_show_error("Incorrect last line."));
 	printf("%s", map_line);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 13:45:06 by xamayuel          #+#    #+#             */
-/*   Updated: 2024/02/18 11:48:19 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/02/18 13:29:49 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ int	main(int argn, char *argv[])
 				Incorrect number of arguments."KNRM"\n");
 	else if (ft_is_valid_input_file(argv) == TRUE)
 	{
-		printf(KGRN"\nLoading map.....\n"KNRM);
-		ft_is_valid_map(argv[1]);
+		printf(KBLU"\nLoading %s ...\n",argv[1]);
+		printf(KNRM);
+		if (ft_is_valid_map(argv[1]) == TRUE)
+			printf(KGRN"Parsing map ...\n"KNRM);
 	}
 	exit(0);
 }

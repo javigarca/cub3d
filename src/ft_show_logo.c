c2868b395ec84cc94b44f04b38743110f4f2474c
xamayuel Mint <unamayu@gmail.com>
2024-02-22 12:27:20 +0100

    docstring

--- src/ft_show_logo.c
+++ src/ft_show_logo.c
@@ -6,12 +6,19 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:10:04 by xamayuel          #+#    #+#             */
-/*   Updated: 2024/02/20 16:22:39 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/02/22 11:56:23 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 
+/**
+ * Displays the game logo in the console.
+ * 
+ * This function prints a stylized logo in the console
+ * using ANSI escape sequences to color the output. 
+ * It also includes the credits of the game developers.
+ */
 void	ft_show_logo(void)
 {
 	printf(""KBLU"  ______  __    __ _______  ________ ");

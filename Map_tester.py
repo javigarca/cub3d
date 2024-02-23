085d5510742299865bb4dbcdbbff1090ee9721eb
1unamayu <unamayu@gmail.com>
2024-02-23 13:07:09 +0100

    cambios en tester

--- Map_tester.py
+++ Map_tester.py
@@ -2,58 +2,63 @@ import os
 import subprocess
 import time
 
-ruta_directorio = "maps/"
+def get_cub_files(directory):
+    """Obtiene una lista de todos los archivos .cub en el directorio."""
+    cub_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".cub"):
+                full_path = os.path.join(root, file)
+                cub_files.append(full_path)
+    return cub_files
 
-# Obtener una lista de todos los archivos en el directorio
-archivos = os.listdir(ruta_directorio)
-
-# Filtrar la lista para obtener solo archivos .cub
-archivos_cub = [archivo for archivo in archivos if archivo.endswith(".cub")]
-
-
-def obtener_archivos_cub(directorio):
-	archivos_cub = []
-	for raiz, subdirectorios, archivos in os.walk(directorio):
-		for archivo in archivos:
-			if archivo.endswith(".cub"):
-				ruta_completa = os.path.join(raiz, archivo)
-				archivos_cub.append(ruta_completa)
-	return archivos_cub
-contador = 1 
-ruta_directorio = "maps/"
-archivos_cub = obtener_archivos_cub(ruta_directorio)
-# Recorrer la lista de archivos .cub
-for archivo_cub in reversed(archivos_cub):
-    # Iniciar el proceso cube3s con el archivo como argumento
-    mensaje = f"TEST {contador}: File {archivo_cub}"
-    print(mensaje, end=' ' * (90 - len(mensaje)))
-    with open("salida.txt", "w") as archivo:
-        proceso = subprocess.Popen(["./cube3d",  archivo_cub], stdout=archivo, stderr=subprocess.STDOUT)
+def run_test(file, test_type, test_number):
+    """Ejecuta una prueba en el archivo dado y escribe el resultado en la salida."""
+    print(f"TEST {test_number} {test_type}: File {file}", end=' ' * (90 - len(f"TEST {test_number} {test_type}: File {file}")))
+    with open(f"salida_{test_type}_{test_number}.txt", "w") as output_file:
+        process = subprocess.Popen(["./cube3d", file], stdout=output_file, stderr=subprocess.STDOUT)
         time.sleep(1)
-        proceso.kill()
-    with open("salida.txt", "r") as archivo:
-        contenido = archivo.read()
-        if "Error" in contenido and "error" in archivo_cub:
+        process.kill()
+    with open(f"salida_{test_type}_{test_number}.txt", "r") as output_file:
+        content = output_file.read()
+        if "Error" in content and "error" in file:
             print("\033[32mOK\033[0m")
-        elif "Error" in contenido and "error" not in archivo_cub:
+        elif "Error" in content and "error" not in file:
             print("\033[31mKO\033[0m")
-        elif "Error" not in contenido and "error" in archivo_cub:
+        elif "Error" not in content and "error" in file:
             print("\033[31mKO\033[0m")
         else:
             print("\033[32mOK\033[0m")
-    mensaje = f"TEST LEAKS {contador}: File {archivo_cub}"
-    print(mensaje, end=' ' * (90 - len(mensaje)))
-    with open(f"salida_leaks_{contador}.txt", "w") as archivo:
-        proceso = subprocess.Popen(["valgrind","./cube3d",  archivo_cub], stdout=archivo, stderr=subprocess.STDOUT)
+
+def run_leaks_test(file, test_number):
+    """Ejecuta una prueba de fugas de memoria en el archivo dado y escribe el resultado en la salida."""
+    print(f"TEST LEAKS {test_number}: File {file}", end=' ' * (90 - len(f"TEST LEAKS {test_number}: File {file}")))
+    with open(f"salida_leaks_{test_number}.txt", "w") as output_file:
+        process = subprocess.Popen(["valgrind","./cube3d", file], stdout=output_file, stderr=subprocess.STDOUT)
         time.sleep(2)
-        proceso.kill()
-    with open(f"salida_leaks_{contador}.txt", "r") as archivo:
-        contenido = archivo.read()
-        if "no leaks are possible" in contenido:
+        process.kill()
+    with open(f"salida_leaks_{test_number}.txt", "r") as output_file:
+        content = output_file.read()
+        if "no leaks are possible" in content:
             print("\033[32mOK\033[0m")
         else:
             print("\033[31mKO\033[0m")
-    contador = contador + 1;
-    time.sleep(2)
-# Imprimir un mensaje al final
-print("Se han analizado todos los archivos .cub")
+def delete_salida_files():
+    """Elimina todos los archivos salida_*.txt en el directorio actual."""
+    for file in os.listdir():
+        if file.startswith("salida_") and file.endswith(".txt"):
+            os.remove(file)
+def main():
+    directory = "maps/"
+    cub_files = get_cub_files(directory)
+    test_number = 1
+    for file in reversed(cub_files):
+        run_test(file, "TEST", test_number)
+        run_leaks_test(file, test_number)
+        test_number += 1
+        time.sleep(2)
+    print("Se han analizado todos los archivos .cub")
+    delete_salida_files()
+
+if __name__ == "__main__":
+    main()

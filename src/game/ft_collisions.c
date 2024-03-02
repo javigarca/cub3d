d44dee5581db351e9603f3983ad5e41dcfa76eb7
xamayuel Mint <unamayu@gmail.com>
2024-03-02 09:55:24 +0100

    norminette en ft_collissions. eliminada funcion no usada

--- src/game/ft_collisions.c
+++ src/game/ft_collisions.c
@@ -6,30 +6,12 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 21:41:12 by javi              #+#    #+#             */
-/*   Updated: 2024/02/29 15:42:05 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/03/02 09:46:10 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
-/**
- * Check if there is a collision at the specified coordinates in the map
- * 
- * @param map The game map
- * @param new The new coordinates to check
- * @return 1 if there is a collision, 0 otherwise
- */
-int	ft_check_collision(t_data *map, t_coord new)
-{
-	int	x;
-	int	y;
-
-	x = (int) new.x;
-	y = (int) new.y;
-	if (map->map2d[x][y] == '1')
-		return (1);
-	return (0);
-}
 /**
  * Check if there is a collision at the X coordinate in the map
  * 
@@ -38,14 +20,18 @@ int	ft_check_collision(t_data *map, t_coord new)
  * @param currentY the current Y coordinate to check
  * @return 1 if there is a collision, 0 otherwise
  */
+int	ft_check_collision_x(t_data *map, double newX, double currentY)
+{
+	int	x;
+	int	y;
 
-int ft_check_collision_x(t_data *map, double newX, double currentY) {
-    int x = (int)newX;
-    int y = (int)currentY;
-    if (map->map2d[x][y] == '1')
-        return 1;
-    return 0;
+	x = (int)newX;
+	y = (int)currentY;
+	if (map->map2d[x][y] == '1')
+		return (1);
+	return (0);
 }
+
 /**
  * Check if there is a collision at the Y coordinate in the map
  * 
@@ -54,10 +40,14 @@ int ft_check_collision_x(t_data *map, double newX, double currentY) {
  * @param currentX the current Y coordinate
  * @return 1 if there is a collision, 0 otherwise
  */
-int ft_check_collision_y(t_data *map, double currentX, double newY) {
-    int x = (int)currentX;
-    int y = (int)newY;
-    if (map->map2d[x][y] == '1')
-        return 1;
-    return 0;
+int	ft_check_collision_y(t_data *map, double currentX, double newY)
+{
+	int	x;
+	int	y;
+
+	x = (int) currentX;
+	y = (int) newY;
+	if (map->map2d[x][y] == '1')
+		return (1);
+	return (0);
 }

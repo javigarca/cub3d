cfe127b0dbfa7c73d9c672fd017cb8931e5de194
xamayuel Mint <unamayu@gmail.com>
2024-03-02 10:33:25 +0100

    norminete sobre ft_mouse

--- src/game/ft_mouse_rot.c
+++ src/game/ft_mouse_rot.c
@@ -3,38 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_mouse_rot.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: javi <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 10:33:40 by javi              #+#    #+#             */
-/*   Updated: 2024/02/29 10:33:58 by javi             ###   ########.fr       */
+/*   Updated: 2024/03/02 10:32:57 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
-int ft_mouse_rotation(int x, int y, t_gamedata *gdata)
+int	ft_mouse_rotation(int x, int y, t_gamedata *gdata)
 {
+	int		delta;
+	int		ypitch;
+	double	normdelta;
 
-    int delta;
-    int ypitch;
-    double normdelta;
-    
-    ypitch = y - (gdata->img_size.y /2);
-    delta = x - (gdata->img_size.x / 2);
-    if (delta != 0)
-     {
-        normdelta = -delta / (gdata->win_size.x / 2.0);
-      //  printf("Rotating: normdelta=%f, rot_speed=%f\n", normdelta, MOUSE_SPEED);
-
-        ft_rotate_player(gdata, normdelta * MOUSE_SPEED);
-    }
-    if (ypitch != 0)
-    {
-        ypitch = fmax(fmin(ypitch, 200.0), -200.0);
-        gdata->player.pitch = ypitch * MOUSE_VERTICAL_SPEED;
-    }
-    //printf("\n pitch: %f", gdata->player.pitch);
-    ft_start_draw(gdata);
-	ft_show_menu(gdata); //CAMBIAR SI QUEREMOS MOSTRAR MAPA
-    return 0;
+	ypitch = y - (gdata->img_size.y / 2);
+	delta = x - (gdata->img_size.x / 2);
+	if (delta != 0)
+	{
+		normdelta = -delta / (gdata->win_size.x / 2.0);
+		ft_rotate_player(gdata, normdelta * MOUSE_SPEED);
+	}
+	if (ypitch != 0)
+	{
+		ypitch = fmax(fmin(ypitch, 200.0), -200.0);
+		gdata->player.pitch = ypitch * MOUSE_VERTICAL_SPEED;
+	}
+	ft_start_draw(gdata);
+	ft_show_menu(gdata);
+	return (0);
 }

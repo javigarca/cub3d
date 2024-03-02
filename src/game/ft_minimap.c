85b4e36e1920cac0cedabad1094f26c0457688e1
xamayuel Mint <unamayu@gmail.com>
2024-03-02 10:18:25 +0100

    norminette sobre minimap

--- src/game/ft_minimap.c
+++ src/game/ft_minimap.c
@@ -3,50 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   ft_minimap.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: javi <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 16:44:46 by javi              #+#    #+#             */
-/*   Updated: 2024/03/01 19:54:50 by javi             ###   ########.fr       */
+/*   Updated: 2024/03/02 10:16:25 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
-void	ft_minimap(t_gamedata *gdata, t_coord origin)
+static void	ft_minimap_player(t_gamedata *data, t_coord origin);
+
+void	ft_minimap_walls_draw(char wall, t_gamedata *gdata, \
+								t_coord start, t_coord end)
 {
-	int	cont;
-	int	col;
-	int	map_lines;
-	t_coord end;
-	t_coord start;
+	if (wall == '1')
+		ft_light_rect(gdata, start, end, MINI_CLR_W);
+	if (wall == '0')
+		ft_light_rect(gdata, start, end, MINI_CLR_F);
+}
+
+void	ft_minimap_walls(t_gamedata *gdata, t_coord origin, int map_lines)
+{
+	int		cont;
+	int		col;
+	t_coord	end;
+	t_coord	start;
 
 	cont = 0;
 	start = origin;
-	map_lines = ft_2d_array_size(gdata->map->map2d);
 	while (cont < map_lines)
 	{
 		col = 0;
 		start.x = (int)origin.x;
-    	while (gdata->map->map2d[cont][col] != '\0')
+		while (gdata->map->map2d[cont][col] != '\0')
 		{
-			end.y = start.y + 6; 
+			end.y = start.y + 6;
 			end.x = start.x + 6;
-			if (gdata->map->map2d[cont][col] == '1')
-				ft_light_rect(gdata, start, end, MINI_CLR_W);
-			else 
-				ft_light_rect(gdata, start, end, MINI_CLR_F);
+			ft_minimap_walls_draw(gdata->map->map2d[cont][col], \
+									gdata, start, end);
 			start.x += 7;
 			end.x += 7;
 			col++;
 		}
-//		start.x = (int)origin.x;
 		start.y += 7;
 		cont++;
 	}
-	start.x = (int)((gdata->player.pos.y) * MINI_SIZE) + origin.x;
-	start.y = (int)((gdata->player.pos.x) * MINI_SIZE) + origin.y;
-	end.x = start.x + 6;
-	end.y = start.y + 6;
-	ft_light_rect(gdata, start, end, P30_CLR);
 }
 
+void	ft_minimap(t_gamedata *gdata, t_coord origin)
+{
+	ft_minimap_walls(gdata, origin, ft_2d_array_size(gdata->map->map2d));
+	ft_minimap_player(gdata, origin);
+}
+
+static void	ft_minimap_player(t_gamedata *data, t_coord origin)
+{
+	t_coord	start;
+	t_coord	end;
+
+	start.x = (int)((data->player.pos.y) * MINI_SIZE) + origin.x;
+	start.y = (int)((data->player.pos.x) * MINI_SIZE) + origin.y;
+	end.x = start.x + 6;
+	end.y = start.y + 6;
+	ft_light_rect(data, start, end, P30_CLR);
+}

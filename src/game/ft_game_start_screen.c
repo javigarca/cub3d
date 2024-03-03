2fe56858858cf6682616d6655b56072035629d25
xamayuel <unamayu@gmail.com>
2024-03-03 19:34:02 +0100

    añadida start screen

--- src/game/ft_game_start_screen.c
+++ src/game/ft_game_start_screen.c
@@ -7,7 +7,7 @@ void ft_game_start_screen(t_gamedata *gdata)
 	int height;
 	void *img_ptr;
 	
-    img_ptr = mlx_xpm_file_to_image(gdata->mlx, "textures/minecraft_512/acacia_planks.xpm", &width, &height);
+    img_ptr = mlx_xpm_file_to_image(gdata->mlx, "textures/startscreen.xpm", &width, &height);
     mlx_put_image_to_window(gdata->mlx, gdata->win, img_ptr, (W_RESOL - width)/2, (H_RESOL - height) / 2 );
 	free(img_ptr);
 }

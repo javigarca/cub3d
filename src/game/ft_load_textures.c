8f3dbc3d1c51562aca32e914e8842dd0740359c0
Javier García-Arango <javi@MacBook-Air-de-Javier.local>
2024-03-04 19:59:24 +0100

    añadidas las supertexturas

--- src/game/ft_load_textures.c
+++ src/game/ft_load_textures.c
@@ -96,9 +96,9 @@ static void	ft_init_texture_img(t_gamedata *data, t_img *image, int type)
 	else if (type == WEST)
 		texture_path = ft_strdup(data->map->t_we.texture_path);
 	else if (type == DOORC)
-		texture_path = ft_strdup("textures/doorclose2.xpm");
+		texture_path = ft_strdup("textures/minecraft_512/reddoor.xpm");
 	else //if (type == DOORO)
-		texture_path = ft_strdup("textures/dooropen2.xpm");
+		texture_path = ft_strdup("textures/minecraft_512/greendoor.xpm");
 	image->img = mlx_xpm_file_to_image(data->mlx, texture_path, \
 										&size_x, &size_y);
 	data->sizee_x[type] = size_x;
